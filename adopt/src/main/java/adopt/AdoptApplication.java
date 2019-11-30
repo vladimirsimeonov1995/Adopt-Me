542d4d20ddec48462ea69d9f3eb7fb84561ab415
Vladimir Simeonov <vladimir.simeonov.1995@gmail.com>
2019-11-30 15:30:08 +0200

    Add the Spring MVC project, only with index page

---
+++ adopt/src/main/java/adopt/AdoptApplication.java
@@ -0,0 +1,13 @@
+package adopt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdoptApplication.class, args);
+    }
+
+}
---
+++ adopt/src/test/java/adopt/AdoptApplicationTests.java
@@ -0,0 +1,13 @@
+package adopt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
